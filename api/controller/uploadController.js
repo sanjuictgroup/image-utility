4d09e8a8fdbcf3f1c771c49c479882ee8b4b0c61
Sanju Panikar <sanju.panikar@ictgroup.ind.in>
2021-07-12 11:19:52 +0530

    Changes

--- api/controller/uploadController.js
+++ api/controller/uploadController.js
@@ -45,6 +45,7 @@ uploadFileToBlob = async (directoryPath) => {
     });
 };
 
+
 const getBlobName = originalName => {
     const identifier = Math.random().toString().replace(/0\./, ''); // remove "0." from start of string
     return `${identifier}-${originalName}`;
